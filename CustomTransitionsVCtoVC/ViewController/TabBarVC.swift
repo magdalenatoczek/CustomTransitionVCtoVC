bfc922bd004e57978e6122bd4b3c3be70cd9955d
Magda Toczek <magdalena.toczek@gmail.com>
2020-09-11 18:17:29 +0200

    custom tab bar transition

--- CustomTransitionsVCtoVC/ViewController/TabBarVC.swift
+++ CustomTransitionsVCtoVC/ViewController/TabBarVC.swift
@@ -1,30 +1,58 @@
 //
-//  TabBarVC.swift
+//  tabBarVC.swift
 //  CustomTransitionsVCtoVC
 //
-//  Created by Magdalena Toczek on 06/09/2020.
+//  Created by Magdalena Toczek on 11/09/2020.
 //  Copyright © 2020 Popeq Apps. All rights reserved.
 //
 
 import UIKit
 
-class TabBarVC: UIViewController {
+class tabBarVC: UITabBarController, UITabBarControllerDelegate {
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        self.delegate = self
     }
     
 
-    /*
-    // MARK: - Navigation
+  
+    
+    func tabBarController(_ tabBarController: UITabBarController, shouldSelect viewController: UIViewController) -> Bool {
+        
+        let selectedViewController = tabBarController.selectedViewController
+        
+        if selectedViewController == nil || viewController == selectedViewController { return false }
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+        guard let controllerIndex = tabBarController.viewControllers?.firstIndex(of: viewController) else { return true }
+        guard let fromView = selectedViewController?.view else{ return true }
+        guard let toView = viewController.view else{ return true }
+        
+        
+        
+        let viewSize : CGRect = fromView.frame
+        let scrolRight : Bool = controllerIndex > tabBarController.selectedIndex
+        fromView.superview?.addSubview(toView)
+        let screenWidth: CGFloat = UIScreen.main.bounds.size.width
+        toView.frame = CGRect(x: scrolRight ? screenWidth : -screenWidth, y: viewSize.origin.y, width: screenWidth, height: viewSize.size.height)
+        
+        UIView.animate(withDuration: 0.5, delay: 0, options: .curveEaseInOut, animations: {
+            
+            fromView.frame = CGRect(x: scrolRight ? -screenWidth: screenWidth, y: viewSize.origin.y, width: screenWidth, height: viewSize.size.height)
+            
+            toView.frame = CGRect(x: 0, y: viewSize.origin.y, width: screenWidth, height: viewSize.size.height)
+            
+        }) { (success) in
+            if success {
+                fromView.removeFromSuperview()
+                tabBarController.selectedIndex = controllerIndex
+            }
+        }
+        
+        
+        return false
     }
-    */
+ 
 
 }
